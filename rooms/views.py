ec7bea81ebb81cb987f2685a7f37c774d8bc294f
unknown <malice@korea.ac.kr>
2022-02-13 02:02:55 +0900

    #2.5 room detail PUT part 2

--- rooms/views.py
+++ rooms/views.py
@@ -55,7 +55,8 @@ class RoomView(APIView):
             serializer = WriteRoomSerializer(
                 room, data=request.data, partial=True)
             if serializer.is_valid():
-                serializer.save()
+                room = serializer.save()
+                return Response(ReadRoomSerializer(room).data)
             else:
                 return Response(serializers.errors, stats=status.HTTP_400_BAD_REQUEST)
             return Response()
