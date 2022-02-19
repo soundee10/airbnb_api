695ac6df3198fb5c0e8c3ff7be54f03fd281cf4c
soundee_laptop <malice@korea.ac.kr>
2022-02-19 17:59:54 +0900

    #2.9 adding/deleting rooms in fav list

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 from rooms import serializers
 from .models import User
+from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from users.serializers import ReadUserSerializer, WriteUserSerialzer
 
@@ -47,4 +48,17 @@ class FavsView(APIView):
         return Response(serializer)
 
     def put(self, request):
-        pass
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.delete(room)
+                else:
+                    user.favs.add(room)
+                return Response()
+            except Room.DoesNotExist:
+                pass
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
