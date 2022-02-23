a75f8bb0fd0d6ff54a8dd156535a737216f3ff83
unknown <malice@korea.ac.kr>
2022-02-24 01:43:34 +0900

    #3.0 Use of SerializerMethod Field

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -6,6 +6,7 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
 
     user = UserSerializer()
+    is_fav = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -23,3 +24,11 @@ class RoomSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "Not enough time between changes")
         return data
+
+    def get_is_fav(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
